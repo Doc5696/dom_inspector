b9303ff9aeb7fe6e37d5598683e4287fc8346de2
petro_brez <petro.brezitskiy@newfireglobal.com>
2024-03-27 20:55:36 +0200

    minor updates

--- src/pages/Fallback/index.tsx
+++ src/pages/Fallback/index.tsx
@@ -3,10 +3,10 @@ import { Stack, Typography } from '@mui/material'
 const Fallback = () => {
   return (
     <Stack>
-      <Typography variant="h3">
+      <Typography variant="h3" align="center">
         Ooops, seems like you are trying to use widget with no parent page
       </Typography>
-      <Typography variant="h4">
+      <Typography variant="h4" align="center" sx={{ p: 5 }}>
         Please check{' '}
         <a href="https://github.com/Doc5696/dom_inspector/blob/master/README.md">
           this page
